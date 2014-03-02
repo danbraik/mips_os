8c65336f85b7c9630d7dbe619782a9632be96565
danbraik <danbraik@hotmail.com>
2014-03-02 16:13:09 +0100

    Add shell files

---
+++ shell.c
@@ -0,0 +1,19 @@
+#ifdef SHELL
+
+#include "simu_mips.h"
+#include "commands.h"
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
+
+
+
+
+
+
+
+
+#endif
---
+++ simu_mips.h
@@ -0,0 +1,9 @@
+#ifndef SIMU_MIPS
+#define SIMU_MIPS
+
+char getc
+
+
+
+
+#endif
